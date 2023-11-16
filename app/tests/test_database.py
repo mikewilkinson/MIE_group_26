bf81caee2ab1f588cf18e389a955027fa3da09e1
FatimaAnwar2 <fatima.anwar-2@postgrad.manchester.ac.uk>
2023-11-16 01:45:42 +0000

    Ifection treatment drugs as %

--- app/tests/test_database.py
+++ app/tests/test_database.py
@@ -35,5 +35,51 @@ class DatabaseTests(unittest.TestCase):
         """Test that avergae calculation returns correct value"""
         self.assertAlmostEqual(self.db_mod.get_average_cost(), 76.22)
 
+    def test_get_items_sum(self,items):
+        """Test the sum of infection treatments with BNF codes 05"""
+        # Sample data for testing
+        sample_data = [
+            {'BNF_code': '0501%', 'items': 10},
+            {'BNF_code': '0502%', 'items': 15},
+            {'BNF_code': '0503%', 'items': 5},
+            {'BNF_code': '0604%', 'items': 20}] # Not starting with '05'
+
+        # Test when there are items with BNF codes 05
+        total_sum = self.db_mod.get_items_sum('05%')
+        expected_sum = sum(item['items'] for item in sample_data if item['BNF_code'].startswith('05'))
+        self.assertEqual(total_sum, expected_sum)
+
+        # Test when there are no items with BNF codes 05
+        total_sum_nonexistent = self.db_mod.get_items_sum('nonexistent_code')
+        self.assertEqual(total_sum_nonexistent, 0)
+
+    def test_percentage_of_infection_drugs(self):
+        """Test the percentage of infection drugs as a percentage of all infection treatments"""
+        # Sample data for testing
+        sample_data = [
+            {'BNF_code': '0501%', 'items': 10},
+            {'BNF_code': '0502%', 'items': 15},
+            {'BNF_code': '0503%', 'items': 5},
+            {'BNF_code': '0604%', 'items': 20}]  # Not starting with '05'
+
+        # Total sum of infection treatments
+        infection_sum = self.db_mod.get_items_sum('05%')
+
+        # Calculate percentages
+        antibacterial_per = format(round(self.db_mod.get_items_sum('0501%') / infection_sum, 4) * 100, '.2f')
+        antifungal_per = format(round(self.db_mod.get_items_sum('0502%') / infection_sum, 4) * 100, '.2f')
+        antiviral_per = format(round(self.db_mod.get_items_sum('0503%') / infection_sum, 4) * 100, '.2f')
+        antiprotozoal_per = format(round(self.db_mod.get_items_sum('0504%') / infection_sum, 4) * 100, '.2f')
+        antihelmintics_per = format(round(self.db_mod.get_items_sum('0505%') / infection_sum, 4) * 100, '.2f')
+
+        # Verify percentages
+        self.assertEqual(antibacterial_per, '33.33', "Expected 33.33 for Antibacterial percentage")
+        self.assertEqual(antifungal_per, '50.00', "Expected 50.00 for Antifungal percentage")
+        self.assertEqual(antiviral_per, '16.67', "Expected 16.67 for Antiviral percentage")
+        self.assertEqual(antiprotozoal_per, '0.00', "Expected 0.00 for Antiprotozoal percentage")
+        self.assertEqual(antihelmintics_per, '0.00', "Expected 0.00 for Antihelmintics percentage")
+
 if __name__ == "__main__":
     unittest.main()
+
+
--- app/views/controllers.py
+++ app/views/controllers.py
@@ -35,10 +35,19 @@ def home():
     bar_labels = bar_data[1]
     title_data_items = generate_data_for_tiles()
 
+    # calculate infection percentages
+    infection_chart_data = generate_infection_barchart_data()
+
+    chart_data = {
+        'tile_data': title_data_items,
+        'pct': {'data': bar_values, 'labels': bar_labels},
+        'pct_list': pcts,
+        'pct_data': selected_pct_data,
+        'infection_chart_data': infection_chart_data
+    }
+
     # render the HTML page passing in relevant data
-    return render_template('dashboard/index.html', tile_data=title_data_items,
-                           pct={'data': bar_values, 'labels': bar_labels},
-                           pct_list=pcts, pct_data=selected_pct_data)
+    return render_template('dashboard/index.html', **chart_data)
 
 def generate_data_for_tiles():
     """Generate the data for the four home page titles."""
@@ -53,4 +62,22 @@ def generate_barchart_data():
     data_values = [r[0] for r in data_values]
     pct_codes = [r[0] for r in pct_codes]
     return [data_values, pct_codes]
+
+def generate_infection_barchart_data():
+    # Calculate the percentage of each specific infection drug
+    infection_sum=db_mod.get_items_sum("05%")
+    antibacterial_per = format(round(db_mod.get_items_sum('0501%') / infection_sum, 4) * 100, '.2f')
+    antifungal_per = format(round(db_mod.get_items_sum('0502%') / infection_sum, 4) * 100, '.2f')
+    antiviral_per = format(round(db_mod.get_items_sum('0503%') / infection_sum, 4) * 100, '.2f')
+    antiprotozoal_per = format(round(db_mod.get_items_sum('0504%') / infection_sum, 4) * 100, '.2f')
+    antihelmintics_per = format(round(db_mod.get_items_sum('0505%') / infection_sum, 4) * 100, '.2f')
+
+    result =  {'Antibacterial': antibacterial_per, 'Antifungal': antifungal_per, 'Antiviral': antiviral_per,
+            'Antoprotozoal': antiprotozoal_per, 'Antihelmintics': antihelmintics_per}
+
+    print(result)
+
+    return result
+
+
 
