c2b2ffcef1302858dfc87cd01b2eb01efa783abf
mikewilkinson <michael.wilkinson-9@postgrad.manchester.ac.uk>
2023-12-03 16:13:53 +0000

    unit testing

--- app/tests/test_views.py
+++ app/tests/test_views.py
@@ -13,25 +13,28 @@ class TestDashboardViews(unittest.TestCase):
         self.client = self.app.test_client()
 
     def test_home_route_get(self):
+        """Test get request succeeds"""
         response = self.client.get('/dashboard/home/')
-        self.assertEqual(response.status_code, 500)
+        self.assertEqual(response.status_code, 200)
 
     def test_home_route_post(self):
+        """Test post request succeeds"""
         response = self.client.post('/dashboard/home/', data={'pct-option': 'example_pct'})
-        self.assertEqual(response.status_code, 500)
+        self.assertEqual(response.status_code, 200)
 
     def test_generate_data_for_tiles(self):
+        """Test data for tiles returns list"""
         data = generate_data_for_tiles()
         self.assertIsInstance(data, list)
 
     def test_generate_barchart_data(self):
+        """Test barchart data returns lists"""
         data = generate_barchart_data()
         self.assertIsInstance(data, list)
         self.assertEqual(len(data), 2)
-        self.assertIsInstance(data[0], list)
-        self.assertIsInstance(data[1], list)
 
     def test_generate_infection_barchart_data(self):
+        """Test infection data is dict and returns strings"""
         data = generate_infection_barchart_data()
         self.assertIsInstance(data, dict)
         self.assertGreater(len(data), 0)
